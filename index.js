35b6acea7153562c688393cf9196568e11d6e70a
shinyrumi <164284874+shinyrumi@users.noreply.github.com>
2024-09-15 15:16:18 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import EmotionCardGame from './EmotionCardGame';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <EmotionCardGame />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
